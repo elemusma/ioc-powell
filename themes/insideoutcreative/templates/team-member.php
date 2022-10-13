d35fb13c2ecd38cdce2a1880bb32273cd227b6e8
Efrain Lemus <efraintlemusm@gmail.com>
2022-10-13 09:07:45 -0600

    update size of job title

--- themes/insideoutcreative/templates/team-member.php
+++ themes/insideoutcreative/templates/team-member.php
@@ -49,7 +49,7 @@ echo '<div class="col-md-6 text-white mb-5 ' . get_sub_field('classes') . '">';
 
 echo wp_get_attachment_image($image['id'],'full','',['class'=>'w-100 h-auto','style'=>'']);
 echo '<p class="h2 bold">' . $name . '</p>';
-echo '<p class="h4 bold">' . $title . '</p>';
+echo '<p class="h5 bold">' . $title . '</p>';
 echo '<a href="mailto:' . $email . '" class="h6">' . $email . '</a>';
 echo '<p class="mb-0">' . $year . '</p>';
 // echo '<p class="h2 bold">' . get_the_title() . '</p>';
